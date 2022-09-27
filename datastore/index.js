f5ab3584f3b8dcfa58b5f962534c27033be7382b
Joseph <jredboyz@gmail.com>
2022-09-26 21:39:05 -0700

    complete retrieving all todos

--- datastore/index.js
+++ datastore/index.js
@@ -9,13 +9,12 @@ const counter = require('./counter');
 exports.create = (text, callback) => {
   counter.getNextUniqueId((err, uniqueId) => {
     var id = uniqueId;
-    // var newPath = path.join(__dirname, 'data', `${id}.txt`);
     var newPath = path.join('test', 'testData', `${id}.txt`);
     fs.writeFile(newPath, text, () => {
       callback(err, { id, text });
     });
   });
-
+  // var newPath = path.join(__dirname, 'data', `${id}.txt`);
   // var id = counter.getNextUniqueId();
   // items[id] = text;
   // callback(null, { id, text });
@@ -23,13 +22,10 @@ exports.create = (text, callback) => {
 
 exports.readAll = (callback) => {
   fs.readdir(exports.dataDir, (err, files) => {
-    var todos = _.map(files, (file) => {
-      fs.readFile(path.join(exports.dataDir, file), (err, data) => {
-        console.log(data, '_____________');
-        return { id: file, text: data.toString() }; // remove txt from id
-      });
+    var todos = files.map((file) => {
+      file = file.slice(0, 5);
+      return { id: file, text: file };
     });
-    console.log(todos);
     callback(err, todos);
   });
   // var data = _.map(items, (text, id) => {
@@ -39,12 +35,12 @@ exports.readAll = (callback) => {
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  // var text = items[id];
+  // if (!text) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback(null, { id, text });
+  // }
 };
 
 exports.update = (id, text, callback) => {
